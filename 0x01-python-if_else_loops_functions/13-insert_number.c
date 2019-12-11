1ad600931a35bfc929a9a70dc6f98b5b5a51341e
amineneifer <amineneifer2000@gmail.com>
2019-12-11 19:23:14 +0000

    efficient nchlh

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,12 +5,6 @@ listint_t *insert_node(listint_t **head, int number)
   listint_t *new;
   listint_t *current;
 
-  if (head == NULL)
-    return(NULL);
-  current =malloc(sizeof(listint_t));
-  if (current == NULL)
-    return (NULL);
-
   current = *head;
   if (*head == NULL)
     {
