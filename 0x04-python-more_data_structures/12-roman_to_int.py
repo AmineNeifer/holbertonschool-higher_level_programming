edcbbf3ec2f2a0f2c64467ca213894c4ddc66b5c
amineneifer <amineneifer2000@gmail.com>
2019-12-19 22:26:52 +0000

    roman isn't scary anymore

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    ad = {'M': 1000}
+    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    numerals.update(ad)
+    result = 0
+    if roman_string:
+        for i in range(len(roman_string)):
+            if i < len(roman_string) - 1:
+                i2 = numerals[roman_string[i+1]]
+                i1 = numerals[roman_string[i]]
+            if i < len(roman_string) - 1 and i2 > i1:
+                result -= numerals[roman_string[i]]
+            else:
+                result += numerals[roman_string[i]]
+        return (result)
+    else:
+        return (0)
