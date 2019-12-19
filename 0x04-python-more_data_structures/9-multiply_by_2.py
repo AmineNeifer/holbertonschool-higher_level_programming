0e1f87162931adc8c16576f8a0ee18a3b2bb9429
amineneifer <amineneifer2000@gmail.com>
2019-12-19 21:06:23 +0000

    used a new dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    for val in new_dict:
-        val *= 2
+        new_dict = a_dictionary.copy()
+        for val in a_dictionary:
+            new_dict[val] = a_dictionary[val] * 2
+        return(new_dict)
