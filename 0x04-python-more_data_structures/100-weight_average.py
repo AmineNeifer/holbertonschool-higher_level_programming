fbae357606cc0462198088dec82faa7bf5066365
amineneifer <amineneifer2000@gmail.com>
2019-12-20 18:29:04 +0000

    look for weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    divi = 0
+    sumi = 0
+    if len(my_list)==0:
+        return (0)
+    for i in my_list:
+        sumi += i[0]*i[1]
+        divi += i[1]
+    return (sumi / divi)
