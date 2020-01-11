27a1f7225b380ce4dbd79fdf528ec54d1c66c811
amineneifer <amineneifer2000@gmail.com>
2020-01-11 16:21:53 +0000

    fixed some if.. elif statements

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,9 +32,9 @@ class Square:
     def position(self, value):
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or type(value[0]) is not int:
+        elif value[0] < 0 or type(value[0]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[1] < 0 or type(value[1]) is not int:
+        elif value[1] < 0 or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
