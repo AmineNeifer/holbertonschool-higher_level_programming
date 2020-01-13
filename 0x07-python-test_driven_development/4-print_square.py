2f482b22a190b476a84ea3e4af25d07ef3b6d2b0
amineneifer <amineneifer2000@gmail.com>
2020-01-13 19:14:49 +0000

    printing squares with '#'

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_square(size):
+#size must be an integer
+    if type(size) is float and (size < 0.0):
+        raise TypeError("size must be an integer")
+    elif type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
