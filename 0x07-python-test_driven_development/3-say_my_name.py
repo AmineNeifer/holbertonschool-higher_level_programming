926746e96b0104288367800bc300b9c5dd841bf6
amineneifer <amineneifer2000@gmail.com>
2020-01-13 18:07:21 +0000

    printing strings and makeing sure they are strings

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" say_my_name is a function that takes two strings, first name and last name 
+    and outputs a message of greeting"""
+def say_my_name(first_name, last_name=""):
+    """Args: first_name: str type.
+             last_name: str type.
+       Returns: Nothing"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {:s} {:s}".format(first_name, last_name))
