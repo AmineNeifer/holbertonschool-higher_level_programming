f48555d62bf3f5f615c3c7471ae247246e863270
amineneifer <amineneifer2000@gmail.com>
2020-01-14 21:48:51 +0000

    added tests on more edge cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,11 +10,12 @@ class TestMaxInteger(unittest.TestCase):
     def test_values(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([4, 2, 1, 3]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([1, 4, 2, 1]), 4)
         self.assertEqual(max_integer([-1, 2, 6, 5]), 6)
         self.assertEqual(max_integer([1, 5, 5, 1]), 5)
         self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)
         with self.assertRaises(TypeError):
-            max_integer([])
             max_integer(["3", 2])
             max_integer([2, 3, 4, 7, "amine"])
