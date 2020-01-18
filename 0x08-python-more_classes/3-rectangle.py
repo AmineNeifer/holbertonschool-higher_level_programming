820fbab41b6eb4418001eb4c661371acf8ec84d3
amineneifer <amineneifer2000@gmail.com>
2020-01-18 21:04:38 +0000

    fix pep8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
+
 class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     def __str__(self):
         if 0 in [self.__width, self.__height]:
             return ""
         chain = self.__width * "#"
         return (chain + "\n") * (self.__height - 1) + chain
-
 
     @property
     def width(self):
@@ -37,7 +37,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
+
     def area(self):
         return (self.__height * self.__width)
 
@@ -45,5 +45,3 @@ class Rectangle:
         if 0 in [self.__height, self.__width]:
             return 0
         return (2 * (self.__height + self.__width))
-
-
