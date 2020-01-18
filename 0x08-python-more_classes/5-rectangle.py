f6efa274acfcbfdf0a9f2bbfe34bb3c14426228d
amineneifer <amineneifer2000@gmail.com>
2020-01-18 21:06:54 +0000

    fix pep8

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
+
 class Rectangle:
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     def __str__(self):
         if 0 in [self.__width, self.__height]:
             return ""
         chain = self.__width * "#"
         return (chain + "\n") * (self.__height - 1) + chain
+
     def __repr__(self):
         return 'Rectangle({}, {})'.format(self.__width, self.__height)
-    
+
     def __del__(self):
         print("Bye rectangle...")
 
@@ -41,7 +44,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
+
     def area(self):
         return (self.__height * self.__width)
 
@@ -49,5 +52,3 @@ class Rectangle:
         if 0 in [self.__height, self.__width]:
             return 0
         return (2 * (self.__height + self.__width))
-    
-
