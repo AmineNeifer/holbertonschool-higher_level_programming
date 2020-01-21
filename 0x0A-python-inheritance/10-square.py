0f8d539e9ad18dbd411fa430f0c881c6547f89f7
amineneifer <amineneifer2000@gmail.com>
2020-01-21 18:12:20 +0000

    added the superclass.__init__

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -32,7 +32,4 @@ class Rectangle(BaseGeometry):
 class Square(Rectangle):
     def __init__(self, size):
         super().integer_validator("size", size)
-        self.__size = size
-      
-    def area(self):
-        return self.__size ** 2
+        Rectangle.__init__(self, size, size)
