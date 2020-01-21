ea267898c11269be4a796e27510f5206b4406db1
amineneifer <amineneifer2000@gmail.com>
2020-01-21 15:55:19 +0000

    check for type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module with one function determines if an object
+    is from a class given or not"""
+def is_same_class(obj, a_class):
+    """ Args: 
+            obj: any object from anytype
+            a_class: like int or str ...
+
+        Returns:
+            True if obj is from type : a_class
+            False otherwise"""
+    return(type(obj) == a_class)
