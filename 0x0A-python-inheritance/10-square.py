c6360d7fce02d2812d44ebffea33a8a7e8bb6baa
amineneifer <amineneifer2000@gmail.com>
2020-01-23 18:23:57 +0000

    configuring inheritance with __import__ from 9-rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    def area(self):
-        """ Raises: Exception: if area is not implemented. """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Raises:
-                TypeError: if value is not int
-                ValueError: if int is less or equal to zero
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Rectangle(BaseGeometry):
