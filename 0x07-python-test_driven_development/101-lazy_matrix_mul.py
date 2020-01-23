61a367ddfa349816dd959c0a8fa43298765e4925
amineneifer <amineneifer2000@gmail.com>
2020-01-23 19:47:47 +0000

    testing raises

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import numpy as np
 def lazy_matrix_mul(m_a, m_b):
+    if m_a is not list:
+        raise TypeError("m_a must be a list")
+    if m_b is not list:
+        raise TypeError("m_a must be a list")
+
     A = np.array(m_a)
     B = np.array(m_b)
     return A.dot(B)
