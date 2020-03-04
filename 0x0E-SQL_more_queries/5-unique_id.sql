2c39543b4cf02658c3eeb7e9bd16247bac7f1db1
amineneifer <amineneifer2000@gmail.com>
2020-03-04 20:21:18 +0000

    added default 1

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,3 @@
 -- table with unique id
-CREATE TABLE IF NOT EXISTS unique_id (id INT(1) UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INTEGER UNIQUE DEFAULT 1,
+name VARCHAR(256));
