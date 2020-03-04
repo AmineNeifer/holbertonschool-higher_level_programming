5b81aec99e5ae46edc368a1a7a42064f4bd15061
amineneifer <amineneifer2000@gmail.com>
2020-03-04 20:42:59 +0000

    rm duplicate database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- create a data base and a table
-CREATE DATABASE IF NOT EXISTS DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
 (id INT AUTO_INCREMENT UNIQUE NOT NULL,
