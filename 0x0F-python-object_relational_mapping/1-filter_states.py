7e5eef499a13b7dd947230b7b939af80be435e55
AmineNeifer <amineneifer2000@gmail.com>
2020-03-28 14:06:08 +0100

    style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import sys
     con = MySQLdb.connect(
         host="localhost", port=3306,
-        user=sys.argv[1],passwd=sys.argv[2],
+        user=sys.argv[1], passwd=sys.argv[2],
         db=sys.argv[3], charset="utf8")
     cur = con.cursor()
     cur.execute("SELECT * FROM states\
