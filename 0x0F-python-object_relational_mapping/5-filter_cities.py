6c61e69ad43f2546b2918ba15baa12edc899a4b0
AmineNeifer <amineneifer2000@gmail.com>
2020-03-29 00:19:51 +0100

    add another if

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,10 +14,14 @@ if __name__ == "__main__":
         INNER JOIN states ON states.id = cities.state_id\
         WHERE states.name=%s", (sys.argv[4],))
     query_rows = cur.fetchall()
+
     for i in range(len(query_rows)):
-        if i == len(query_rows) - 1:
+        if len(query_rows) == 0:
+            print()
+        elif i == len(query_rows) - 1:
             print(query_rows[i][0])
         else:
             print(query_rows[i][0], end=", ")
+        print()
     cur.close()
     con.close()
