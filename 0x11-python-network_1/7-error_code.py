4978db0d036ac82eb5e97f4f0ca1b2c149ec0965
AmineNeifer <amineneifer2000@gmail.com>
2020-04-16 15:16:02 +0100

    output correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,4 +5,4 @@ sends a request to the URL and displays the body of the response.
 import sys
 import requests
 if __name__ == "__main__":
-    print(requests.get(sys.argv[1]).status_code)
+    print("Error code:", requests.get(sys.argv[1]).status_code)
