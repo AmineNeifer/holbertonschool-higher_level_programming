4213877654d538e8c253798e261b7d73f3c332b1
AmineNeifer <amineneifer2000@gmail.com>
2020-04-16 13:51:22 +0100

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+fetch a url using requests module
+"""
+import requests
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
