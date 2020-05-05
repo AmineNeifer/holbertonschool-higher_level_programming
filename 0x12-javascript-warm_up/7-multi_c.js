4908358e46472b5babd7159a0d9892e2e5eabae3
AmineNeifer <amineneifer2000@gmail.com>
2020-05-05 19:55:50 +0100

    fix styling

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ let i;
 if (isNaN(myArg) || !myArg) {
   console.log('Missing number of occurrences');
 } else {
-  for (i = 0; i < Number(myArg) ; i++) {
-      console.log('C is fun');
+  for (i = 0; i < Number(myArg); i++) {
+    console.log('C is fun');
   }
 }
