e7a86a433f565070c0c2be73145e1e99b054ae84
AmineNeifer <amineneifer2000@gmail.com>
2020-05-05 20:10:21 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 const myArg = process.argv.slice(2)[0];
-let letter;
+const letter = 'X';
 let i;
-let j;
 if (isNaN(myArg) || !myArg) {
   console.log('Missing size');
 } else {
   for (i = 0; i < Number(myArg); i++) {
-    letter = 'X';
-    for (j = 1; j < Number(myArg); j++) {
-      letter = letter + letter;
-    }
-    console.log(letter);
+    console.log(letter.repeat(Number(myArg)));
   }
 }
