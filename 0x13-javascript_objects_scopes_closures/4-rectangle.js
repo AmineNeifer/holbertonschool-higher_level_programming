2cdc263614b4e67b3aa164d3c8c69403bcc630b5
AmineNeifer <amineneifer2000@gmail.com>
2020-05-11 23:36:51 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    const x = 'X';
+    for (i = 0; i < this.height; i++) {
+      process.stdout.write(x.repeat(this.width) + '\n');
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+module.exports = Rectangle;
