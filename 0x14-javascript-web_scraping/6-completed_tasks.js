5e0793e43ab83b726493b36940012ebd4b61537d
AmineNeifer <amineneifer2000@gmail.com>
2020-05-12 18:42:47 +0100

    semistandard style

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,14 @@
 const request = require('request');
 const url = process.argv.slice(2)[0];
 request(url, function (error, response, body) {
-  if (error) { console.log(error); } 
-  else 
-  {
-    data = JSON.parse(body);
-    result = {}
-    for (let job of data)
-    {
-      if (result[job.userId] == undefined)
-      {
+  if (error) { console.log(error); } else {
+    const data = JSON.parse(body);
+    const result = {};
+    for (const job of data) {
+      if (result[job.userId] == null) {
         result[job.userId] = 0;
       }
-      if (job["completed"] === true)
-      {
+      if (job.completed === true) {
         result[job.userId]++;
       }
     }
