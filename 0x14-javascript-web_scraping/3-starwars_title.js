538083214d8b2f10029f57d2fc1f4c5024270db5
AmineNeifer <amineneifer2000@gmail.com>
2020-05-12 18:47:05 +0100

    used process.stdout.write instead of console.log

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,6 +4,6 @@ const arg = process.argv.slice(2)[0];
 const url = 'https://swapi-api.hbtn.io/api/films/' + arg;
 request(url, function (error, response, body) {
   if (error) { throw (error); } else {
-    console.log(JSON.parse(body).title);
+    process.stdout.write(JSON.parse(body).title);
   }
 });
