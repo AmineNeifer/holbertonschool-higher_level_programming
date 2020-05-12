032e33f4385333ac1fbbaf25a5b4f7fe352017aa
AmineNeifer <amineneifer2000@gmail.com>
2020-05-12 18:46:03 +0100

    handled error in another way

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const request = require('request');
 const arg = process.argv.slice(2)[0];
 const url = 'https://swapi-api.hbtn.io/api/films/' + arg;
 request(url, function (error, response, body) {
-  if (error) { console.log(error); } else {
+  if (error) { throw (error); } else {
     console.log(JSON.parse(body).title);
   }
 });
