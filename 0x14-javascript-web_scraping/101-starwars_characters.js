0bddc4b36df4149f28385fd6585a2f32a6cc69ce
AmineNeifer <amineneifer2000@gmail.com>
2020-05-13 10:47:02 +0100

    scnd adv task

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+request(url, function (error, response, body) {
+  if (error) { console.log(error); } else {
+    const listChar = JSON.parse(body).characters;
+    for (let i = 0; i < listChar.length; i++) {
+      const actor = listChar[i];
+      request(actor, function (error, response, body) {
+        if (error) { console.log(error); } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
